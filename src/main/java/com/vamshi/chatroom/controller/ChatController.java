73546f17bfbb4e256d148bd11682caca0a733947
vamshi9603 <vamshich87@gmail.com>
2024-08-26 00:44:04 +0530

    updated code to add user and remove

--- src/main/java/com/vamshi/chatroom/controller/ChatController.java
+++ src/main/java/com/vamshi/chatroom/controller/ChatController.java
@@ -1,28 +1,49 @@
 package com.vamshi.chatroom.controller;
 
 import com.vamshi.chatroom.model.Message;
+import com.vamshi.chatroom.model.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
-public class ChatController
-{
+@Slf4j
+public class ChatController {
+
     @Autowired
-    SimpMessagingTemplate simpMessagingTemplate;
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    // ConcurrentHashMap to keep track of online users
+    private final Set<String> onlineUsers = ConcurrentHashMap.newKeySet();
 
     @MessageMapping("/message")
-    @SendTo("/chatroom/public")
-    public Message receivePublicMessage(@Payload Message message ){  // /app/message
-        return message;
+    public void receivePublicMessage(@Payload Message message) {
+        // Handle public message
+        log.info("Received public message: {}", message);
+        if ( message.getStatus() == Status.JOIN) {
+            onlineUsers.add(message.getSenderName());
+            broadcastOnlineUsers();
+        } else if (message.getStatus() == Status.LEAVE) {
+            onlineUsers.remove(message.getSenderName());
+            broadcastOnlineUsers();
+        }
+        simpMessagingTemplate.convertAndSend("/chatroom/public", message);
     }
 
     @MessageMapping("/private-message")
-    public Message receivePrivateMessage(@Payload Message message ){
-        simpMessagingTemplate.convertAndSendToUser( message.getReceiverName(),"/private",message );  // /user/{username}/private
-        return message;
+    public void receivePrivateMessage(@Payload Message message) {
+        log.info("Received private message: {}", message);
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
+    }
+
+    private void broadcastOnlineUsers() {
+        // Broadcast the list of online users to all connected clients
+        simpMessagingTemplate.convertAndSend("/online-users", onlineUsers);
     }
 }
